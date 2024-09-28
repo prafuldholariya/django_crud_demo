1c1295fd69e04b14df132056ecce9820500ef325
Praful Dholariya <praful.dholariya@devputers.com>
2024-09-28 13:22:48 +0530

    Implement edit and delete views in items/views.py and related templates.

--- crud_demo/items/urls.py
+++ crud_demo/items/urls.py
@@ -8,4 +8,6 @@ app_name = 'items'
 urlpatterns = [
     path('', views.item_list, name='item_list'),
     path('create/', views.item_create, name='item_create'),
+    path('<int:pk>/edit/', views.item_update, name='item_edit'),
+    path('<int:pk>/delete/', views.item_delete, name='item_delete'),
 ]
--- crud_demo/items/views.py
+++ crud_demo/items/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -17,3 +17,23 @@ def item_create(request):
     else:
         form = ItemForm()
     return render(request, 'item_form.html', {'form': form})
+
+
+def item_update(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('items:item_list')
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'item_form.html', {'form': form})
+
+
+def item_delete(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('items:item_list')
+    return render(request, 'item_confirm_delete.html', {'item': item})
