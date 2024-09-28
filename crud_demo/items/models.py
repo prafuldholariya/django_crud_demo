96465ec2b1e0faaba39ecd9613156791e4abd020
Praful Dholariya <praful.dholariya@devputers.com>
2024-09-28 11:21:22 +0530

    Define the Item model in items/models.py and create and apply migrations.

--- crud_demo/items/models.py
+++ crud_demo/items/models.py
@@ -1,3 +1,11 @@
+# items/models.py
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
